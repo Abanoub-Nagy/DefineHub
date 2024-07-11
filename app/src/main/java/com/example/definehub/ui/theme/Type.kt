bb1530e388f6d5247839ef29685ee013c8dae7aa
abano <abanoubnagy24@gmail.com>
2024-07-11 15:57:44 +0300

    implement code ui and make app run successfully

--- app/src/main/java/com/example/definehub/ui/theme/Type.kt
+++ app/src/main/java/com/example/definehub/ui/theme/Type.kt
@@ -2,18 +2,22 @@ package com.example.definehub.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.definehub.R
+
+
+val font = FontFamily(
+    Font(R.font.font, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = font,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
